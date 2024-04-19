2d2ac6890391d20c816949a40a2a2428a2d07cf0
Sergei <delusional.drago@gmail.com>
2024-04-20 00:16:12 +0800

    Starting to use Spring MVC

---
+++ src/main/java/com/example/marsroverapiapp/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.marsroverapiapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getHomeView() {
+        return "index";
+    }
+}
