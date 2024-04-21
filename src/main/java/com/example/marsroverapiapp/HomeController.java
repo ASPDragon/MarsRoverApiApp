a2c96fc41592a2af3365e38fc9860b576c453c1e
Sergei <delusional.drago@gmail.com>
2024-04-21 19:21:23 +0800

    Deep Dive on Spring Models

--- src/main/java/com/example/marsroverapiapp/HomeController.java
+++ src/main/java/com/example/marsroverapiapp/HomeController.java
@@ -1,12 +1,23 @@
 package com.example.marsroverapiapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String getHomeView() {
+    public String getHomeView(ModelMap model) {
+        model.put("name", "Sergei Page");
+        model.put("address", "123, Fake St.");
+        return "index";
+    }
+
+    @GetMapping("/testing")
+    public String getTestView(ModelMap model) {
+        model.put("name", "John Doe");
+        model.put("address", "1234, Fake St.");
         return "index";
     }
 }
