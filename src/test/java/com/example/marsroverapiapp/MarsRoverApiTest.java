9d82163e5f9b185ec10bfff92a1308dec93499e5
Sergei <delusional.drago@gmail.com>
2024-05-04 14:11:08 +0800

    RestTemplate and Junit, Mapping JSON Response to Java Objects

--- src/test/java/com/example/marsroverapiapp/MarsRoverApiTest.java
+++ src/test/java/com/example/marsroverapiapp/MarsRoverApiTest.java
@@ -1,10 +1,17 @@
 package com.example.marsroverapiapp;
 
+import com.example.marsroverapiapp.response.MarsRoverApiResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class MarsRoverApiTest {
     @Test
     public void smallTest() {
-        System.out.println("Hello there!");
+        RestTemplate rt = new RestTemplate();
+
+        String url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=nvhWBLej0XW9KGq9gQD1kbLd4SLDIasE7dkUg64I";
+        ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity(url, MarsRoverApiResponse.class);
+        System.out.println(response.getBody());
     }
 }
